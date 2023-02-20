6c55ca8bafd2bf770cae8e02fcaac6ae95c3fb87
vesnatodor <vesnaatodorovic@gmail.com>
2023-02-20 13:15:58 +0100

    Create a video element

---
+++ videoElement/js/other.js
@@ -0,0 +1,8 @@
+//Create a video element in the background of the page
+// and a div over it.
+const myTimeout = setTimeout(Start, 1000);
+function Start(){
+document.querySelector('#video').play();
+}
+
+
